a26d56c1e178c5b7a42492f2dd5dfd222685a8b0
SuperCoolNinja <sbpro570@gmail.com>
2024-06-22 21:11:30 +0200

    init

---
+++ Inspection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionInspection
+{
+    internal class Inspection
+    {
+        private object _objectToInspect;
+        public Inspection(object objectToInspect)
+        {
+            _objectToInspect = objectToInspect;
+        }
+
+        public void GetAllProperties()
+        {
+            Type objectType = _objectToInspect.GetType();
+
+            if (objectType != null)
+            {
+                PropertyInfo[] properties = objectType.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+
+                Console.WriteLine("Properties");
+
+                foreach (var p in properties)
+                {
+                    Console.WriteLine($"\t {p.Name} {p.GetValue(_objectToInspect)}");
+                }
+            }
+        }
+
+        public void GetAllFields()
+        {
+            Type objectType = _objectToInspect.GetType();
+            if (objectType != null)
+            {
+                FieldInfo[] fields = objectType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+                Console.WriteLine("Fields");
+
+                foreach (var f in fields)
+                {
+                    Console.WriteLine($"\t {f.Name} {f.GetValue(_objectToInspect)}" );
+                }
+            }
+        }
+
+        public void GetAllMethods()
+        {
+            Type objectType = _objectToInspect.GetType();
+            if (objectType != null)
+            {
+                MethodInfo[] methods = objectType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+
+                Console.WriteLine("Methods");
+
+                foreach (var m in methods)
+                {
+                    Console.WriteLine("\t" + m);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ReflectionInspection
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        private int age => 25;
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Inspection inspect = new(new User { Name = "Ninja" });
+            inspect.GetAllFields();
+            inspect.GetAllProperties();
+            inspect.GetAllMethods();
+        }
+    }
+}
